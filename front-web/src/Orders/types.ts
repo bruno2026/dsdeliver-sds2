d35b49b04da935935be45021318bacb69889e5f1
Bruno Gomes <brunoo.gomes11@gmail.com>
2021-01-08 23:10:58 -0300

    Search order location

---
+++ front-web/src/Orders/types.ts
@@ -0,0 +1,13 @@
+export type Product ={
+        id: number;
+        name: string;
+        price: 38.0;
+        description: string;
+        imageUri: string;
+}
+
+export type OrderLocationData = {
+        latitude:number;
+        longitude:number;
+        address: string;
+}
