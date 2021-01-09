c34a38cccd69a075d68dce8cbe75bf47500bea33
Bruno Gomes <brunoo.gomes11@gmail.com>
2021-01-09 19:11:54 -0300

    Alter footer icons

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -1,23 +1,24 @@
 import './styles.css';
-import {ReactComponent as YoutubeIcon} from './youtube.svg';
+
 import {ReactComponent as LinkedinIcon} from './linkedin.svg';
-import {ReactComponent as InstagramIcon} from './instagram.svg';
+
+import {ReactComponent as GitHubIcon} from './github.svg';
 
 function Footer() {
     return (
         <footer className="main-footer">
             App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
             <div className="footer-icons">
-                <a href="https://www.youtube.com/c/DevSuperior" target="_new">
-                    <YoutubeIcon />
+                <a href="https://github.com/bruno2026" target="_new">
+                    <GitHubIcon />
                 </a>
 
-                <a href="https://linkedin.com/school/devsuperior/" target="_new">
+                <a href="https://www.linkedin.com/in/brunogomes2021/" target="_new">
                     <LinkedinIcon />
                 </a>
-                <a href="https://www.instagram.com/devsuperior.ig">
-                    <InstagramIcon />
-                </a>
+
+
+                
             </div>
         </footer>
     )
